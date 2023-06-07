65901f1c3e54b952bdb54da1d6b2a947deee9676
Abdul-Mueed-Shahbaz <bscs18056@itu.edu.pk>
2023-06-08 01:47:51 +0500

    posts

--- anime_tracker_api/apps/watchlist/api/views.py
+++ anime_tracker_api/apps/watchlist/api/views.py
@@ -1,3 +1,8 @@
+import json
+
+from django.shortcuts import get_object_or_404
+from rest_framework import exceptions, status
+from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from .serializers import WatchlistSerializer
@@ -14,3 +19,21 @@ class WatchListViewSet(ModelViewSet):
         'patch'
     ]
     queryset = WatchList.objects.all()
+
+    @action(methods=['post'], detail=True, url_name='add-to-watchlist', url_path='add-to-watchlist')
+    def add_watchlist(self, request, pk=None):
+        user_watchlist = get_object_or_404(WatchList, pk=pk)
+        anime_list = request.data.get('anime_list')
+        if not anime_list:
+            raise exceptions.NotAcceptable
+        if not user_watchlist.anime_list:
+            user_watchlist.anime_list = anime_list
+            user_watchlist.save()
+            data = self.get_serializer(user_watchlist).data
+            return Response({
+                'watchlist': data,
+            }, status=status.HTTP_200_OK)
+        data = self.get_serializer(user_watchlist).data
+        return Response({
+            'watchlist': data
+        })
