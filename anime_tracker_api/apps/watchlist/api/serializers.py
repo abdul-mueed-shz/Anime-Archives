8fba6ea058a83fb6d4ee23a7a4aecfb993a9d749
Abdul-Mueed-Shahbaz <bscs18056@itu.edu.pk>
2023-06-09 01:18:56 +0500

    add to watchlist

--- anime_tracker_api/apps/watchlist/api/serializers.py
+++ anime_tracker_api/apps/watchlist/api/serializers.py
@@ -12,3 +12,36 @@ class WatchlistSerializer(serializers.ModelSerializer):
             'name',
             'anime_list'
         ]
+
+
+class SingleKeyListField(serializers.Field):
+    def to_internal_value(self, data):
+        anime_list = data.get('anime_list')
+        if not isinstance(data, dict) or len(data) != 1 or not anime_list:
+            raise serializers.ValidationError('Invalid data format. Must be a dictionary with a single key.')
+
+        key = next(iter(data))
+        value = data[key]
+
+        if not isinstance(value, list):
+            raise serializers.ValidationError('Invalid value. Must be a list.')
+
+        return {key: value}
+
+    def to_representation(self, value):
+        return value
+
+
+class AnimeListSerializer(serializers.Serializer):
+    data = SingleKeyListField()
+
+    def validate_data(self, value):
+        key = next(iter(value))
+        if not isinstance(value[key], list):
+            raise serializers.ValidationError('Invalid value. Must be a list.')
+        return value
+
+    def validate(self, attrs):
+        if 'data' not in attrs:
+            raise serializers.ValidationError('anime_list is required.')
+        return attrs
--- anime_tracker_api/apps/watchlist/api/views.py
+++ anime_tracker_api/apps/watchlist/api/views.py
@@ -1,3 +1,4 @@
+import ast
 import json
 
 from django.shortcuts import get_object_or_404
@@ -5,7 +6,7 @@ from rest_framework import exceptions, status
 from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
-from .serializers import WatchlistSerializer
+from .serializers import WatchlistSerializer, AnimeListSerializer
 from ..models import WatchList
 
 
@@ -20,20 +21,37 @@ class WatchListViewSet(ModelViewSet):
     ]
     queryset = WatchList.objects.all()
 
+    def validate_list(self, request):
+        AnimeListSerializer(data=request.data).is_valid(raise_exception=True)
+
     @action(methods=['post'], detail=True, url_name='add-to-watchlist', url_path='add-to-watchlist')
     def add_watchlist(self, request, pk=None):
+        def add_anime_to_list(data, anime_list_dict):
+            anime_list = anime_list_dict.get('anime_list')
+            for anime in data:
+                if anime not in anime_list:
+                    anime_list.append(anime)
+
         user_watchlist = get_object_or_404(WatchList, pk=pk)
-        anime_list = request.data.get('anime_list')
-        if not anime_list:
+        anime_data_dict = request.data.get('data')
+        if not anime_data_dict:
             raise exceptions.NotAcceptable
         if not user_watchlist.anime_list:
-            user_watchlist.anime_list = anime_list
+            self.validate_list(request)
+            user_watchlist.anime_list = anime_data_dict
             user_watchlist.save()
             data = self.get_serializer(user_watchlist).data
+            data['anime_list'] = anime_data_dict
             return Response({
                 'watchlist': data,
             }, status=status.HTTP_200_OK)
+        self.validate_list(request)
+        watchlist_obj_anime_list = ast.literal_eval(user_watchlist.anime_list)
+        add_anime_to_list(anime_data_dict.get('anime_list'), watchlist_obj_anime_list)
+        user_watchlist.anime_list = watchlist_obj_anime_list
+        user_watchlist.save()
         data = self.get_serializer(user_watchlist).data
+        data['anime_list'] = watchlist_obj_anime_list
         return Response({
             'watchlist': data
         })
